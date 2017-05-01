61f92c7d9885716759aeb47cddcebe4cf56ed245
tnepper <Talia@MacBook-2.local>
2017-05-01 12:31:35 -0500

    Dev tool initial commit

---
+++ lib/granzow_view_tool.rb
@@ -0,0 +1,6 @@
+require "granzow_view_tool/version"
+require "granzow_view_tool/renderer"
+
+module GranzowViewTool
+  # Your code goes here...
+end
---
+++ lib/granzow_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GranzowViewTool
+  VERSION = "0.1.0"
+end
